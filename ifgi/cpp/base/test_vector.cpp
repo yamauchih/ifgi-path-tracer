071a0703d1e1627cf2838ae3babd39796eb6f931
Yamauchi Hitoshi <yamauchi@sunday.researcher>
2011-12-29 00:52:40 +0100

    Replace unit test facility to google test.

--- ifgi/cpp/base/test_vector.cpp
+++ ifgi/cpp/base/test_vector.cpp
@@ -1,19 +1,25 @@
 //----------------------------------------------------------------------
-// test_Vector
+// test_vector: google test
 // Copyright (C) 2010-2011 Yamauchi, Hitoshi
 //----------------------------------------------------------------------
 /// \file
 /// \brief Vector test
 
-#define BOOST_TEST_MODULE Vector_test test
-#include <boost/test/unit_test.hpp>
-
 #include "Vector.hh"
 
-BOOST_AUTO_TEST_CASE( Vector_test )
+#include <gtest/gtest.h>
+
+// Tests Vector
+TEST(VectorTest, Vectos)
 {
     ifgi::Float32_3 v0(1.0f, 0.0f, 0.0f);
     ifgi::Float32_3 v1(0.0f, 1.0f, 0.0f);
 
     ifgi::Float32_3 v2 =  cross(v0, v1);
+}
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
---
+++ tool/replace-python-to-cpp.el
@@ -0,0 +1,115 @@
+;;
+;; Utility functions of transforming python file to cpp file
+;; Copyright (C) 2011 Yamauchi Hitoshi
+;;
+
+;; symbol list
+(setq python-cpp-symbol-replace-list
+      (list
+       '("self, "  "")                  ; remove 'self, '
+       '("self\\." "this->")            ; 'self.' -> 'this->'
+       '("self"    "")                  ; remove 'self'
+       '("\"\"\""  "/// ")
+       '("# "      "/// ")
+       '("\'"      "\"")
+       '("):"      "){")
+       '(")$"      ");")
+       '("\\$"      "")                 ; remove trailing backslash
+       '(" _"       " ")                ;
+
+
+       ;; ;; QFileDialog
+       ;; '("QFileDialog(\\(.*\\),[ \t\n]*\\(\".*\"\\),[ \t\n]*\\(.*\\))"
+       ;;   "QFileDialog(\\1);\npO->setObjectName(\\2);\npO->setModal(\\3);")
+       ;; '("setMode(QFileDialog::\\(.*\\))"
+       ;;   "setFileMode(QFileDialog::\\1)")
+
+       ;; ;; sstreamcomp
+       ;; '("base/sstreamcomp.hh" "sstream")
+
+       ;; ;;
+       ;; '("gmu/Incoming/hitoshi/Geometry/MST" "gmu/Tools/MST")
+       ;; '("gmu/Incoming/hitoshi/Common/DP"    "gmu/Common/DataProc")
+       ;; '("gmu/Incoming/hitoshi/Common/ModVisMeshScalar.hh"
+       ;;   "gmu/Common/VisMeshScalar/ModVisMeshScalar.hh")
+       ;; '("gmu/Incoming/hitoshi/Common/VMS"   "gmu/Common/VisMeshScalar")
+       ;; '("gmu/Incoming/hitoshi/Common"       "gmu/Common")
+       ;; '("GMU::Geometry::Dijkstra"    "GMU::MST::Dijkstra")
+       ))
+
+(defun replace-python-to-cpp (b e)
+  "Replace python symbols to cpp symbols"
+  (interactive "r")
+  (save-excursion
+    (save-restriction
+      (narrow-to-region b e)
+      (let
+	  ((python-sym-list python-cpp-symbol-replace-list))
+	(while python-sym-list
+	  (let ((org (car (car python-sym-list)))
+		(dst (car (cdr (car python-sym-list)))))
+	    (goto-char b)
+	    (replace-regexp org dst)
+	    (setq python-sym-list (cdr python-sym-list))))))))
+
+(defun query-replace-python-to-cpp (b e)
+  "Query replace python symbols to cpp symbols"
+  (interactive "r")
+  (save-excursion
+    (save-restriction
+      (narrow-to-region b e)
+      (let
+	  ((python-sym-list python-cpp-symbol-replace-list))
+	(while python-sym-list
+	  (let ((org (car (car python-sym-list)))
+		(dst (car (cdr (car python-sym-list)))))
+	    (goto-char b)
+	    (query-replace-regexp org dst)
+	    (setq python-sym-list (cdr python-sym-list))))))))
+
+
+;;
+;; insert cpp header
+;;
+(defun insert-c++-header ()
+  "insert c++ header
+"
+  (interactive)
+  (let* (
+         (abspath  (buffer-file-name))
+         (fname    (file-name-nondirectory abspath))
+         (incguard (upcase abspath))
+         (ns-name  "ifgi")
+        )
+    ;; I could not find direct string manipuration, so use a temporary
+    ;; buffer. This is powerful, but, I feel a bit overkill.
+    ;; make a include guard from absolute pathname.
+    (with-temp-buffer
+      (progn
+        (insert-string incguard)
+        ;; special for ifgi. Cut the project path
+        (replace-string "/home/hitoshi/data/project/" "" nil (point-min) (point-max))
+        ;; in general
+        (replace-string "/" "_" nil (point-min) (point-max))
+        (replace-string "-" "_" nil (point-min) (point-max))
+        (replace-string "." "_" nil (point-min) (point-max))
+        (setq incguard (buffer-substring (point-min) (point-max)))
+        ))
+    (insert-string
+     (format
+      "//----------------------------------------------------------------------
+// ifgi c++ implementation: %s
+// Copyright (C) 2010-2011 Yamauchi, Hitoshi
+//----------------------------------------------------------------------
+/// \\file
+/// \\brief %s
+#ifndef %s
+#define %s
+namespace %s
+{
+} // namespace %s
+#endif // #ifndef %s
+" fname fname incguard incguard ns-name ns-name incguard))))
+
+
+
