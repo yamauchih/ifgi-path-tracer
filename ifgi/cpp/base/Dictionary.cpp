6a498afebe6404c036ad8a4ec290d29e3747fd02
yamauchi.hitoshi <yamauchi@sunday.researcher>
2012-01-02 23:09:13 +0100

    Pre-commit before changed Dict name.

--- ifgi/cpp/base/Dictionary.cpp
+++ ifgi/cpp/base/Dictionary.cpp
@@ -6,8 +6,89 @@
 /// \brief key value map with type conversion
 
 #include "Dictionary.hh"
+#include "Exception.hh"
 
 namespace ifgi {
+
+//======================================================================
+// any value -> string conversion
+//======================================================================
+
+// dictonary conversion functions namespace
+namespace dictconv {
+
+/// general vector to string method
+/// \param[val] vec_val vector value
+/// \return string representation of vec_val
+template < typename T, int DIM >
+static inline std::string vector_value_to_string(
+    Vector< T, DIM > const & vec_val)
+{
+    std::ostringstream osstr;
+    for(int i = 0; i < (vec_val.dim() - 1); ++i){
+        osstr << vec_val[i] << " ";
+    }
+    osstr << vec_val[vec_val.dim() - 1];
+    return osstr.str();
+}
+
+//----------------------------------------------------------------------
+// Float32_3 -> string conversion
+template <>
+std::string value_to_string< Float32_3 >(Float32_3 const & vec)
+{
+    return vector_value_to_string< Float32, 3 >(vec);
+}
+
+//======================================================================
+// string -> any value conversion
+//======================================================================
+
+/// general string to vector conversion method
+///
+/// throw an Exception when conversion failed.
+///
+/// \param[in] vec_str vector string
+/// \return type vector representation of vec_str
+
+#define IFGI_MAKE_STRING_FROM_SYMBOL_X(s) IFGI_MAKE_STRING_FROM_SYMBOL(s)
+#define IFGI_MAKE_STRING_FROM_SYMBOL(s) #s
+
+template < typename T, int DIM >
+static inline Vector< T, DIM > string_to_vector_value(
+    std::string const & vec_str) 
+{
+    Vector< T, DIM > ret;
+    std::istringstream isstr(vec_str);
+    for(int i = 0; i < ret.dim(); ++i){
+        isstr >> ret[i];
+        if(isstr.fail()){
+            std::stringstream sstr;
+            sstr << "failed string to vector conversion. ["
+                 << vec_str + "] to Vector< "
+                 << std::string(IFGI_MAKE_STRING_FROM_SYMBOL_X(T)) << ", "
+                 << DIM << " >.";
+            throw Exception(sstr.str());
+        }
+    }
+    return ret;
+}
+
+#undef IFGI_MAKE_STRING_FROM_SYMBOL_X
+#undef IFGI_MAKE_STRING_FROM_SYMBOL
+
+//----------------------------------------------------------------------
+// string -> Float32_3 conversion
+template <>
+Float32_3 string_to_value< Float32_3 >(std::string const & val_str)
+{
+    Float32_3 const ret = string_to_vector_value< Float32, 3 >(val_str);
+    return ret;
+}
+
+//----------------------------------------------------------------------
+} // namespace dictconv
+
 
 //----------------------------------------------------------------------
 // constructor
--- ifgi/cpp/base/Dictionary.hh
+++ ifgi/cpp/base/Dictionary.hh
@@ -3,39 +3,104 @@
 // Copyright (C) 2010-2011 Yamauchi, Hitoshi
 //----------------------------------------------------------------------
 /// \file
-/// \brief key value map with type conversion
+/// \brief python dict like class
 #ifndef IFGI_PATH_TRACER_IFGI_CPP_BASE_DICTIONARY_HH
 #define IFGI_PATH_TRACER_IFGI_CPP_BASE_DICTIONARY_HH
 
 #include <map>
 #include <string>
-// #include <iostream>
+#include <sstream>
+
+#include "types.hh"
+#include "Vector.hh"
 
 namespace ifgi {
 
-/// value string. The entry of Dictionary.
+/// dictonary conversion functions namespace
+namespace dictconv {
+
+//======================================================================
+// any value -> string conversion
+//======================================================================
+//----------------------------------------------------------------------
+/// general dictionary value to string conversion template function.
+/// However, supported types should be specialized.
 ///
-/// This is a value and can be converted to many other type if
+/// \param[in] val value of type T
+/// \return string representation of val
+template < typename T >
+std::string value_to_string(T const & val)
+{
+    std::ostringstream osstr;
+    osstr << val;
+    return osstr.str();
+}
+
+//----------------------------------------------------------------------
+/// Float32_3 -> string conversion
+/// \param[in] vec a vector of Float32_3
+/// \return string representation of Float32_3
+template <>
+std::string value_to_string< Float32_3 >(Float32_3 const & vec);
+
+//======================================================================
+// string -> any value conversion
+//======================================================================
+
+//----------------------------------------------------------------------
+/// general string to dictionary value conversion template function.
+/// However, supported types should be specialized.
+template < typename T >
+T string_to_value(std::string const & str)
+{
+    T ret;
+    std::ostringstream osstr;
+    osstr >> ret;
+    return ret;
+}
+
+//----------------------------------------------------------------------
+/// string -> Float32_3 conversion
+/// \param[in] vec_str vector value
+/// \return converted Float32_3
+template <>
+Float32_3 string_to_value< Float32_3 >(std::string const & vec_str);
+
+//----------------------------------------------------------------------
+} // namespace dictconv
+
+
+/// dictionary value. The entry of Dictionary.
+///
+/// This is a value and can be converted to many other types if
 /// possible. This has a string to keep the value.
 ///
-// class ValueString
-// {
-// public:
-//     /// constructor
-//     ValueString();
-//     /// destructor
-//     virtual ~ValueString();
+class Dictionary_value
+{
+public:
+    /// constructor
+    Dictionary_value();
 
-// private:
-//     /// value kept in a string
-//     std::string m_value;
-// };
+    /// constructor with supported type
+    ///
+    /// \param[in] val type T value, T must be supported in the
+    /// conversion functions.
+    template < typename T >
+    explicit Dictionary_value(T const & val);
+
+    /// destructor
+    virtual ~Dictionary_value();
+
+private:
+    /// value kept in a string
+    std::string m_value;
+};
 
 /// dictionary, key value map with type conversion
 class Dictionary
 {
 public:
-    /// 
+    ///
     typedef std::map< std::string, std::string > Dictionary_map;
 
 public:
@@ -53,15 +118,18 @@ public:
 
     /// insert key and value
     /// pair<iterator, bool>
-    void insert(std::string const & key, std::string const & val);
+    void insert(std::string const & key, Dictionary_value const & val);
 
     /// get the value as a string
     std::string get(std::string const & key,
                     std::string const & default_val = "") const;
 
     /// get the value
-    // template < typename T > T get(std::string const & key,
-    //                               std::string const & default_val = "") const ;
+    template < typename T > T get(std::string const & key,
+                                  std::string const & default_val = "") const
+    {
+
+    }
 
     // /// output parameters to stream, format: <line_prefix><key> = <value>
     // void write(std::ostream &os, std::string const & prefix) const;
